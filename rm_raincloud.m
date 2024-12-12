a598249f5966164b56860d4ecd82f7fdc1fe5c4b
Arne Hansen <arne96.hansen@gmail.com>
2024-12-12 14:47:46 +0100

    working version rainclouds

--- rm_raincloud.m
+++ rm_raincloud.m
@@ -1,4 +1,4 @@
-function h = rm_raincloud(data, colours, plot_top_to_bottom, raindrop_size, plot_mean_dots)
+function h = rm_raincloud(data, colours, plot_top_to_bottom, raindrop_size, plot_mean_dots, connecting_lines)
 %% Check dimensions of data (should be M x 2 where M is the number of subjects, 2 for low and high contrast)
 [n_subjects, n_conditions] = size(data);
 
@@ -66,29 +66,39 @@ hold on
 for conds = 1:n_conditions
     for subj = 1:n_subjects
         h.p{subj, conds} = patch('Faces', faces{subj, conds}, 'Vertices', verts{subj, conds}, 'FaceVertexCData', colours(conds, :), 'FaceColor', 'flat', 'EdgeColor', 'none', 'FaceAlpha', 0.5);
-        
+
         % Modify scatter points to mirror condition 2
         if conds == 2 % Mirror the scatter points of the second condition by negating the y-values
-            h.s{subj, conds} = scatter(data{subj, conds}, -jit{subj, conds} - ks_offsets(conds), 'MarkerFaceColor', colours(conds, :), 'MarkerEdgeColor', 'none', 'MarkerFaceAlpha', 0.5, 'SizeData', raindrop_size);
+            h.s{subj, conds} = scatter(data{subj, conds}, -jit{subj, conds} + ks_offsets(conds-1)-1, 'MarkerFaceColor', colours(conds, :), 'MarkerEdgeColor', 'none', 'MarkerFaceAlpha', 0.5, 'SizeData', raindrop_size);
         else
             h.s{subj, conds} = scatter(data{subj, conds}, -jit{subj, conds} + ks_offsets(conds), 'MarkerFaceColor', colours(conds, :), 'MarkerEdgeColor', 'none', 'MarkerFaceAlpha', 0.5, 'SizeData', raindrop_size);
         end
     end
 end
 
-% Plot mean lines
-for conds = 1:n_conditions
-    for subj = 1:n_subjects - 1
-        h.l(subj, conds) = line(cell_means([subj, subj+1], conds), ks_offsets([subj, subj+1]), 'LineWidth', 4, 'Color', colours(conds, :));
-    end
-end
+% Plot grey lines connecting data from condition 1 to condition 2 for each participant
+% if connecting_lines == 1
+%     for subj = 1:n_subjects
+%         % Only draw a line if both conditions have data for this participant
+%         if ~isempty(data{subj, 1}) && ~isempty(data{subj, 2})
+%             % Get the mean data points for the participant in both conditions
+%             x1 = data{1}(subj);
+%             x2 = data{2}(subj);
+%             y1 = -jit{subj, conds} - ks_offsets(conds);
+%             y2 = -jit{subj, conds} + ks_offsets(conds);
+% 
+%             % Plot a thin grey line with alpha 0.5
+%             h.line{subj} = plot([x1, x2], [y1, y2], 'Color', [0.5 0.5 0.5], 'LineWidth', 0.5, 'LineStyle', '-', 'Marker', 'none', 'MarkerEdgeColor', 'none');
+%         end
+%     end
+% end
 
 % Plot mean dots
 if plot_mean_dots == 1
     for conds = 1:n_conditions
         for subj = 1:n_subjects
             if conds == 2 % Mirror the mean dots for the second condition by negating the y-values
-                h.m(subj, conds) = scatter(cell_means(subj, conds), -ks_offsets(conds), 'MarkerFaceColor', colours(conds, :), 'MarkerEdgeColor', [0 0 0], 'MarkerFaceAlpha', 1, 'SizeData', raindrop_size * 1.5, 'LineWidth', 2);
+                h.m(subj, conds) = scatter(cell_means(subj, conds), -ks_offsets, 'MarkerFaceColor', colours(conds, :), 'MarkerEdgeColor', [0 0 0], 'MarkerFaceAlpha', 1, 'SizeData', raindrop_size * 1.5, 'LineWidth', 2);
             else
                 h.m(subj, conds) = scatter(cell_means(subj, conds), ks_offsets(conds), 'MarkerFaceColor', colours(conds, :), 'MarkerEdgeColor', [0 0 0], 'MarkerFaceAlpha', 1, 'SizeData', raindrop_size * 1.5, 'LineWidth', 2);
             end
