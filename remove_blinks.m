d72e0c3bca4e598a0f596ca5867573dc83588f39
Arne Hansen <arne96.hansen@gmail.com>
2025-01-07 11:34:34 +0100

    Update remove_blinks.m

--- remove_blinks.m
+++ remove_blinks.m
@@ -1,6 +1,6 @@
 %% Function to remove blinks with sliding window
 function cleaned_data = remove_blinks(data, window_size)
-blink_indices = find(all(data(1:2, :) == 0, 1));
+blink_indices = find(data(2, :) < 150);
 removal_indices = [];
 for i = 1:length(blink_indices)
     start_idx = max(1, blink_indices(i) - window_size);
