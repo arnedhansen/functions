e95105080dc2958f14e210e97ad0a86830621147
Arne Hansen <arne96.hansen@gmail.com>
2025-03-05 16:16:03 +0100

    Delete baseline_corr_powspctrm.m

--- baseline_corr_powspctrm.m
+++
@@ -1,8 +0,0 @@
-%% Baseline correction for power spectra for TFR
-
-function freqGA_data = baseline_corr_powspctrm(freqGA_data, baseline_period)
-    % Find baseline period in data from ft_freqgrandaverage
-    baseline_idx = find(freqGA_data.time >= baseline_period(1) & freqGA_data.time <= baseline_period(2));
-    baseline_power = mean(freqGA_data.powspctrm(:,:,baseline_idx), 3, 'omitnan');
-    freqGA_data.powspctrm = ((freqGA_data.powspctrm - baseline_power) ./ baseline_power) * 100;
-end
