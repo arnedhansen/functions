f38cba9256a290b91e380a2e5d4b46ce91ef449b
Arne Hansen <arne96.hansen@gmail.com>
2025-03-26 18:06:27 +0100

    setup with ISPC

--- setup.m
+++ setup.m
@@ -1,31 +1,35 @@
 %% Function to set up data processing for any project
 
 function [subjects, path, colors] = setup(projectName)
-    % Clear environment
-    clearvars -except projectName;
-    addpath('/Users/Arne/Documents/matlabtools/eeglab2024.2');
-    eeglab;
-    clc;
-    close all;
+% Clear environment
+clearvars -except projectName;
 
-    % Load colors
-    colors = color_def(projectName);
+% Add EEG Lab functions
+addEEGLab
 
-    % Set the base path according to the provided project name
+% Load colors
+colors = color_def(projectName);
+
+% Set the base path according to the provided project name
+if ispc == 1
+    baseDir = 'W:\Students\Arne\';
+    path = strcat(baseDir, projectName, 'data\features\');
+else
     baseDir = '/Volumes/methlab/Students/Arne/';
     path = fullfile(baseDir, projectName, 'data/features/');
+end
 
-    % Check if the path exists
-    if ~isfolder(path)
-        error('The specified project path does not exist: %s', path);
-    end
+% Check if the path exists
+if ~isfolder(path)
+    error('The specified project path does not exist: %s', path);
+end
 
-    % List directories in the selected path
-    dirs = dir(path);
-    folders = dirs([dirs.isdir] & ~ismember({dirs.name}, {'.', '..'}));
-    subjects = {folders.name};
+% List directories in the selected path
+dirs = dir(path);
+folders = dirs([dirs.isdir] & ~ismember({dirs.name}, {'.', '..'}));
+subjects = {folders.name};
 
-    % Display the loaded subjects
-    disp('Loaded subjects:');
-    disp(subjects);
+% Display the loaded subjects
+disp('Loaded subjects:');
+disp(subjects);
 end
