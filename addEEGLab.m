bcecfa0fec8566da5490e3a4cb53d8ab04fad90b
Arne Hansen <arne96.hansen@gmail.com>
2025-09-29 09:40:07 +0200

    addEEGLab change paths

--- addEEGLab.m
+++ addEEGLab.m
@@ -2,7 +2,8 @@ function addEEGLab
 if ispc == 1
     addpath W:\Students\Arne\toolboxes\eeglab2024.2
 else
-    addpath /Volumes/methlab/Students/Arne/toolboxes/eeglab2024.2
+    addpath /Volumes/g_psyplafor_methlab$/Students/Arne/toolboxes/eeglab2024.2
+    %addpath /Users/Arne/Documents/MATLAB/eeglab2024.2
 end
 eeglab
 clc
