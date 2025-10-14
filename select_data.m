ba57336fa70c011b63dc2ae994eb4833ea087ca5
Arne Hansen <arne96.hansen@gmail.com>
2025-10-14 13:43:34 +0200

    Update select_data.m

--- select_data.m
+++ select_data.m
@@ -1,8 +1,17 @@
 %% Function to select data in FieldTrip structures
-function selected_data = select_data(cfg_latency, cfg_frequency, data)
-    cfg = [];
-    cfg.latency = cfg_latency;
-    cfg.frequency = cfg_frequency;
-    cfg.avgovertime = 'yes';
+function selected_data = select_data(latency, frequency, data)
+    cfg              = [];
+    cfg.latency      = latency;
+    cfg.frequency    = frequency;
+    cfg.avgovertime  = 'yes';
+    cfg.avgoverfreq  = 'no';
+    cfg.avgoverchan  = 'no';
+    cfg.nanmean      = 'yes';
+
     selected_data = ft_selectdata(cfg, data);
+
+    % If time artifacts remain, prune safely
+    if isfield(selected_data, 'time') || (isfield(selected_data, 'dimord') && contains(selected_data.dimord,'time'))
+        selected_data = remove_time_dimension(selected_data);
+    end
 end
