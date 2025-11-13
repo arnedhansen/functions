3ff2afd86468e2fc333c78b098c47e05f472d9e5
Arne Hansen <arne96.hansen@gmail.com>
2025-11-13 16:09:22 +0100

    setup with EEGLab/FieldTrip exclusion

--- setup.m
+++ setup.m
@@ -2,9 +2,12 @@
 %
 % Syntax:
 %   [subjects, path, colors, headmodel] = setup(projectName)
+%   [subjects, path, colors, headmodel] = setup(projectName, initToolboxes)
 %
 % Input:
-%   projectName - (char) Name of the project folder containing subject data.
+%   projectName    - (char) Name of the project folder containing subject data.
+%   initToolboxes  - (logical / numeric) Flag indicating whether to initialise
+%                    EEGLAB and FieldTrip. Default = 1.
 %
 % Output:
 %   subjects   - (cell array) Names of subject folders in the data directory.
@@ -16,26 +19,34 @@
 %   - color_def
 %   - addEEGLab
 
-function [subjects, path, colors, headmodel] = setup(projectName)
-% Clear environment
-clearvars -except projectName;
+function [subjects, path, colors, headmodel] = setup(projectName, initToolboxes)
 
-% Add EEG Lab functions
-clc
-disp(upper('adding eeglab functions...'))
-addEEGLab
-
-% Initialize FieldTrip
-clc
-disp(upper('initializing FieldTrip...'))
-if ispc == 1
-    addpath('W:\Students\Arne\toolboxes\fieldtrip-20250928');
-else
-    addpath('/Volumes/g_psyplafor_methlab$/Students/Arne/toolboxes/fieldtrip-20250928');
+% Handle optional input
+if nargin < 2 || isempty(initToolboxes)
+    initToolboxes = 1; % default: load EEGLAB + FieldTrip
 end
-% which ft_defaults
-% which ft_warning
-ft_defaults
+
+% Clear environment (keep inputs)
+clearvars -except projectName initToolboxes;
+
+% Add EEG Lab functions and initialise FieldTrip only if requested
+if initToolboxes
+    clc
+    disp(upper('adding eeglab functions...'))
+    addEEGLab
+
+    clc
+    disp(upper('initializing FieldTrip...'))
+    if ispc == 1
+        addpath('W:\Students\Arne\toolboxes\fieldtrip-20250928');
+    else
+        addpath('/Volumes/g_psyplafor_methlab$/Students/Arne/toolboxes/fieldtrip-20250928');
+    end
+    ft_defaults
+else
+    clc
+    disp(upper('skipping eeglab and fieldtrip initialization...'))
+end
 
 % Load colors
 colors = color_def(projectName);
@@ -65,11 +76,10 @@ end
 dirs = dir(path);
 folders = dirs([dirs.isdir] & ~ismember({dirs.name}, {'.', '..'}));
 subjects = {folders.name};
-%subjects = subjects(1:10)
-%disp('ATTENTION ONLY 10 SUBS LOADED ATTENTION ONLY 10 SUBS LOADED ATTENTION ONLY 10 SUBS LOADED ATTENTION ONLY 10 SUBS LOADED ATTENTION ONLY 10 SUBS LOADED ATTENTION ONLY 10 SUBS LOADED ATTENTION ONLY 10 SUBS LOADED')
 
 % Display the loaded subjects
 filteredSubjects = str2double(string(subjects));
 disp('Loaded subjects:');
 disp(filteredSubjects(:));
+
 end
